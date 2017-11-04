15c75512db8122c3a6e5aaf308d2116bd30d95ee
Diogo Lima <srdiogolima@gmail.com>
2017-11-04 16:46:32 -0200

    - Completing tasks 5 to 9
    - Adding title, badges and url
    - Add avatar of gravatar
    - Add score

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,50 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/srdiogolima.json', // My report card
+    dataType: 'jsonp',
+    success: function(response) {
+      addCompletedCourses(response.courses.completed);
+      addAvatar(response.user);
+      addScore(response.user);
+    }
+  })
 
-});
+  function addCompletedCourses(courses) {
+    $badges = $('#badges');//handle response
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+  }
+
+  function addAvatar(user){
+    $('<img />', {
+      src: user.avatar
+    }).appendTo($('#avatar'));
+  }
+
+  function addScore(user){
+    $('<h2 />', {
+      id: 'score',
+      text: 'My Score:'+user.total_score
+    }).appendTo($('#mySettings'));
+  }
+})
